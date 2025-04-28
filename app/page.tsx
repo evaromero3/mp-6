43438c8267eb14b67ad5b7038c25527cfc898aeb
Eva Romero <eromero3@bu.edu>
2025-04-27 21:35:47 -0400

    fixing vercel issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 
 import { useEffect, useState } from "react";
 import SignInButton from "../components/SignInButton";
-import { DialogContent } from "@mui/material";
 
 export default function Home() {
   const [user, setUser] = useState<{
